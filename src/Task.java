83fadc01c75e5df1450a0ce9ffd52de4bf06253b
Wenbin Su <swbrrobin@gmail.com>
2021-03-21 23:23:19 +1100

    Add some comments to code

--- src/Task.java
+++ src/Task.java
@@ -20,62 +20,77 @@ public class Task {
 		this.isComplete = isComplete;
 	}
 	
+	//Get the description of the task
 	public String getDescription() {
 		return description;
 	}
 	
+	//Set the description of the task
 	public void setDescription(String description) {
 		this.description = description;
 	}
 	
+	//Determine whether the task is complete
 	public boolean isComplete() {
 		return isComplete;
 	}
 	
+	//Set the complete status to the task
 	public void setComplete(boolean isComplete) {
 		this.isComplete = isComplete;
 	}
 	
+	//Set the assignee to to the task
 	public void setAssignTask(String assignee) {
 		this.assignee = assignee;
 	}
 	
+	//Set the duration of the task
 	public void setTaskDuration(int duration) {
 		this.duration = duration;
 	}
 	
+	//Get who is assigned to do the task
 	public String getAssignTask() {
 		return assignee;
 	}
 	
+	//Get the duration of the task
 	public int getTaskDuration() {
 		return duration;
 	}
 	
+	//Get the resources required to complete the task
 	public HashSet<String> getResources() {
 		return resources;
 	}
 	
+	//Set the resources required to complete the task
 	public void setResources(HashSet<String> resources) {
 		this.resources = resources;
 	}
 	
+	//Add more resources required to complete the task
 	public void addResources(HashSet<String> resources) {
 		this.resources.addAll(resources);
 	}
 
+	//Set the prioritization of the task
 	public void setPrioritization(int prioritize) {
 		this.prioritize = prioritize;
 	}
 	
+	//Get the prioritization of the task
 	public int getPrioritization() {
 		return prioritize;
 	}
 	
+	//Set the classification of the task
 	public void setClassification(String classification) {
 		this.classification = classification;
 	}
 	
+	//Get the classification of the task
 	public String getClassification() {
 		return classification;
 	}
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -5,25 +5,38 @@ import java.util.HashMap;
 public class ToDoList {
 private HashMap<String, Task> tasks = new HashMap<String, Task>();
 
+	//Add a task to the ToDoList
 	public void addTask (Task task) {
 		tasks.put(task.getDescription(),task);
 	}
+	
+	//Complete a task matching the description
 	public void completeTask(String description) {
 		tasks.get(description).isComplete();
 	}
+	
+	//Get the complete status of the task that matches the description
 	public boolean getStatus(String description) {
 		return tasks.get(description).isComplete();
 	}
+	
+	//Get the task matching the description
 	public Task getTask(String description) {
 		return tasks.get(description);
 	}
+	
+	//Remove a task matching the description
 	public Task removeTask(String description) {
 		return tasks.remove(description);
 	}
+	
+	//Get all the tasks in the ToDoList
 	public Collection<Task> getAllTasks() {
 		return tasks.values();
 
 	}
+	
+	//Get all the uncompleted tasks in the ToDoList
 	public Collection<Task> getUncompletedTasks() {
 		Collection<Task> uncompletedTasks = new ArrayList<Task> ();
 		Collection<Task> allTasks = new ArrayList<Task> ();
